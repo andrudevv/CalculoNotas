8371976605dc3cbb08c43b766cee7690c83d2616
andrudevv <rvandruzyzz@gmail.com>
2023-02-08 21:32:10 -0500

    ultimos detalles

--- app.js
+++ app.js
@@ -1,50 +1,77 @@
-const calcular = document.getElementById('btn-calcular');
+const calculate = document.getElementById('btn-calcular');
+calculate.addEventListener('click',function conditional() {
+    let number1 = document.getElementById('num1');
+    let number2 = document.getElementById('num2');
+    let number3 = document.getElementById('num3');
+    let number1Average = number1.value *(.30);
+    let number2Average = number2.value *(.30);
+    let number3Average = number3.value *(.40);
+    let total = document.querySelector('.total');
+    let subject = document.querySelector('.subject');
+    const sumAverage = parseFloat(number1Average+number2Average+number3Average);
+    let retrieve = document.querySelector('.ned1');
+    let minWin = document.querySelector('.ned2');
+    let winWithHonors = document.querySelector('.ned3');
+    total.innerHTML = `Nota Final. ${sumAverage}`;
 
-calcular.addEventListener('click',function calcularPromedio() {
-    const noNecNotas=0;
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
-    const num3 = document.getElementById('num3').value;
-    let promdnum1 = parseFloat(num1)* .30;
-    let promdnum2 = parseFloat(num2)* .30;
-    let promdnum3 = parseFloat(num3)* .40;
-    const recuperar = document.querySelector('.ned1');
-    const minimoParaGanar = document.querySelector('.ned2');
-    const ganarconHonores = document.querySelector('.ned3');
-    const total = document.querySelector('.total');
-    const perdioAsignatura = document.querySelector('.perdioAsinatura')
-    const sumPromedio = parseFloat(promdnum1+promdnum2+promdnum3);
-    total.innerHTML = `Nota Final. ${sumPromedio}`;
-
-    if(sumPromedio <= 2.0) {
-        perdioAsignatura.innerHTML =  `Perdiste la asignatura`;
-        recuperar.innerHTML = `Si deseas recuperar, Necesitas ${ (2.1) - sumPromedio} `;
-        minimoParaGanar.innerHTML = `Para ganar satisfactoriamente necesitarias minimo ${(3.5) - sumPromedio  }`;
-        ganarconHonores.innerHTML = `Para ganar con Honores, Necesitas minimo ${(4.6) - sumPromedio}`;
-
-    }else if(sumPromedio >= 2.1 && sumPromedio <= 3.4 ) {
-        perdioAsignatura.innerHTML =  `Perdiste la asignatura, Pero puedes recuperar`;
-        recuperar.innerHTML = ``;
-        minimoParaGanar.innerHTML = `Para ganar satisfactoriamente necesitarias minimo ${(3.5) - sumPromedio  }`;
-        ganarconHonores.innerHTML = `Para ganar con Honores, Necesitas minimo ${(4.6) - sumPromedio}`;
-    }else if(sumPromedio >= 3.5 && sumPromedio <= 4.5) {
-        perdioAsignatura.innerHTML =  `Ganaste la asignatura`;
-        recuperar.innerHTML = `No necesitas notas para recuperar ${noNecNotas} `;
-        minimoParaGanar.innerHTML = `Ganaste satisfactoriamente la asignatura ${noNecNotas}`;
-        ganarconHonores.innerHTML = `Para ganar con Honores, Necesitas minimo ${(4.6) - sumPromedio}`;
-    }else if (sumPromedio >= 4.6 && sumPromedio <= 5.0) {
-        perdioAsignatura.innerHTML =  `Ganaste la asignatura`;
-        recuperar.innerHTML = ``;
-        minimoParaGanar.innerHTML = `Ganaste satisfactoriamente la asignatura ${noNecNotas}`;
-        ganarconHonores.innerHTML = `Ganaste con Honores la asignatura ${noNecNotas}`;
+    if (sumAverage < 2.0) {
+        subject.innerHTML =  `Perdiste la asignatura`;
+        retrieve.innerHTML = ``;
+        minWin.innerHTML = ``;
+        winWithHonors.innerHTML = ``;
+    }else if (sumAverage > 2.0) {
+        subject.innerHTML =  `Ganaste la asignatura`;
+        retrieve.innerHTML = ``;
+        minWin.innerHTML = ``;
+        winWithHonors.innerHTML = ``;
     }
-    
-
 });
 
 
-// document.getElementById("myBtn").addEventListener("click", displayDate);
-// element.addEventListener("click", function(){ alert("Hello World!"); });
-// const sumTotal = promdnum1 + promdnum2 + promdnum3;
 
-// const valorTotal = parseFloat(num1 + num2 + num3)
+function calculateAverage() {
+    let noNotes=0;
+    let number1 = document.getElementById('num1');
+    let number2 = document.getElementById('num2');
+    let number3 = document.getElementById('num3');
+    let number1Average = number1.value *(.30);
+    let number2Average = number2.value *(.30);
+    let number3Average = number3.value *(.40);
+    let retrieve = document.querySelector('.ned1');
+    let minWin = document.querySelector('.ned2');
+    let winWithHonors = document.querySelector('.ned3');
+    let total = document.querySelector('.total');
+    let subject = document.querySelector('.subject');
+
+    if (isNaN(num3)) {
+        num3 = 0;
+    }
+    
+    const sumAverage = parseFloat(number1Average+number2Average+number3Average);
+    total.innerHTML = `Nota Final. ${sumAverage}`;
+
+    if(sumAverage <= 2.0) {
+        subject.innerHTML =  `Perdiste la asignatura`;
+        retrieve.innerHTML = `Si deseas recuperar, Necesitas ${ (2.1) - sumAverage} `;
+        minWin.innerHTML = `Para ganar satisfactoriamente necesitarias minimo ${(3.5) - sumAverage  }`;
+        winWithHonors.innerHTML = `Para ganar con Honores, Necesitas minimo ${(4.6) - sumAverage}`;
+
+    }else if(sumAverage >= 2.1 && sumAverage <= 3.4 ) {
+        subject.innerHTML =  `Perdiste la asignatura, Pero puedes recuperar`;
+        retrieve.innerHTML = ``;
+        minWin.innerHTML = `Para ganar satisfactoriamente necesitarias minimo ${(3.5) - sumAverage  }`;
+        winWithHonors.innerHTML = `Para ganar con Honores, Necesitas minimo ${(4.6) - sumAverage}`;
+    }else if(sumAverage >= 3.5 && sumAverage <= 4.5) {
+        subject.innerHTML =  `Ganaste la asignatura`;
+        retrieve.innerHTML = `No necesitas notas para recuperar ${noNotes} `;
+        minWin.innerHTML = `Ganaste satisfactoriamente la asignatura ${noNotes}`;
+        winWithHonors.innerHTML = `Para ganar con Honores, Necesitas minimo ${(4.6) - sumAverage}`;
+    }else if (sumAverage >= 4.6 && sumAverage <= 5.0) {
+        subject.innerHTML =  `Ganaste la asignatura`;
+        retrieve.innerHTML = ``;
+        minWin.innerHTML = `Ganaste satisfactoriamente la asignatura ${noNotes}`;
+        winWithHonors.innerHTML = `Ganaste con Honores la asignatura ${noNotes}`;
+    }
+    
+
+}
