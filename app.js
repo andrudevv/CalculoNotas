f3f64fb30071e683a49cb15d3d4aab2f700dc51a
andrudevv <rvandruzyzz@gmail.com>
2023-02-12 23:38:00 -0500

    finalizando detalles de calculos

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ calculate.addEventListener('click',function conditional() {
     let retrieve = document.querySelector('.ned1');
     let minWin = document.querySelector('.ned2');
     let winWithHonors = document.querySelector('.ned3');
-
+    let repeatText = (subject.innerHTML =  ``) + (retrieve.innerHTML = ``) + (minWin.innerHTML = ``) + (winWithHonors.innerHTML = ``);
     let number1Average = number1.value *(.30);
     let number2Average = number2.value *(.30);
     let number3Average = number3.value *(.40);
@@ -19,32 +19,22 @@ calculate.addEventListener('click',function conditional() {
     
     total.innerHTML = `Nota Final. ${(sumAverage).toFixed(2)}`;
 
-    if(sumAverage <= 2.099){
+    if(sumAverage <= 2.09){
         subject.innerHTML =  `Lo sentimos, Perdiste la asignatura.`;
-        retrieve.innerHTML = ``;
-        minWin.innerHTML = ``;
-        winWithHonors.innerHTML = ``;
-
-    } else if (sumAverage >= 2.1 || sumAverage < 3.5) {
-        subject.innerHTML =  `Perdiste la asignatura, Pero tienes oportunidad de Recuperar`;
-        retrieve.innerHTML = ``;
-        minWin.innerHTML = ``;
-        winWithHonors.innerHTML = ``;
-    }else if (sumAverage >= 3.5 || sumAverage <=4.59) {
-        subject.innerHTML =  `Ganaste la asignatura`;
-        retrieve.innerHTML = ``;
-        minWin.innerHTML = ``;
-        winWithHonors.innerHTML = ``;
-    }else if (sumAverage >=4.6) {
+        repeatText;
+    }else if(sumAverage >= 2.10 && sumAverage <= 3.49) {
+        subject.innerHTML =  `Perdiste la asignatura, Pero tienes oportunidad de Recuperar.`;
+        repeatText;
+    }else if(sumAverage >= 3.5 && sumAverage <= 4.59) {
+        subject.innerHTML =  `Ganaste satisfactoriamente la asignatura.`;
+        repeatText;
+    }else if(sumAverage >=4.6) {
         subject.innerHTML =  `Ganaste la asignatura con Honores.`;
-        retrieve.innerHTML = ``;
-        minWin.innerHTML = ``;
-        winWithHonors.innerHTML = ``;
+        repeatText;
     }
 });
 
 function calculateAverage() {
-    let noNotes=0;
     let number1Average = number1.value *(.30);
     let number2Average = number2.value *(.30);
     let retrieve = document.querySelector('.ned1');
@@ -58,25 +48,26 @@ function calculateAverage() {
 
     if( sumAverage <= 0.1) {
         subject.innerHTML =  `Lo sentimos, Perdiste la asignatura.`;
-        retrieve.innerHTML = ``;
-        minWin.innerHTML = ``;
-        winWithHonors.innerHTML = ``;
-        
-    }else if( sumAverage >= 2.1  || sumAverage <= 3.4) {
+    }else if (sumAverage >= 0.2 && sumAverage<= 2) {
         subject.innerHTML =  `Vas perdiendo la asignatura, Pero puedes recuperar`;
         retrieve.innerHTML = `Si deseas recuperar, Necesitas ${((2.1 - sumAverage) / 0.4).toFixed(2) }`;
-        minWin.innerHTML = `Para ganar satisfactoriamente necesitarias minimo ${((3.5 - sumAverage) / 0.4).toFixed(2) }`;
+        minWin.innerHTML = `Para ganar satisfactoriamente, necesitarias minimo ${((3.5 - sumAverage) / 0.4).toFixed(2) }`;
         winWithHonors.innerHTML = `Para ganar con Honores, Necesitas minimo ${((4.6 - sumAverage) / 0.4).toFixed(2) }`;
-    }else if(sumAverage >= 3.5 || sumAverage <= 4.5) {
+    }else if( sumAverage >= 2.01  && sumAverage <= 3.49) {
+        subject.innerHTML =  `Puedes recuperar`;
+        retrieve.innerHTML = `No necesitas notas para recuperar`;
+        minWin.innerHTML = `Para ganar satisfactoriamente, necesitarias minimo ${((3.5 - sumAverage) / 0.4).toFixed(2) }`;
+        winWithHonors.innerHTML = `Para ganar con Honores, Necesitas minimo ${((4.6 - sumAverage) / 0.4).toFixed(2) }`;
+        
+    }else if(sumAverage >= 3.5 && sumAverage <= 4.59) {
         subject.innerHTML =  `Ganaste la asignatura`;
-        retrieve.innerHTML = `No necesitas notas para recuperar ${noNotes} `;
-        minWin.innerHTML = `Ganaste satisfactoriamente la asignatura ${noNotes}`;
+        retrieve.innerHTML = `No necesitas notas para recuperar`;
+        minWin.innerHTML = `Ganaste satisfactoriamente la asignatura`;
         winWithHonors.innerHTML = `Para ganar con Honores, Necesitas minimo ${((4.6 - sumAverage)  / 0.4).toFixed(2)}`;
     }else if (sumAverage >= 4.6) {
         subject.innerHTML =  `Ganaste la asignatura`;
-        retrieve.innerHTML = ``;
-        minWin.innerHTML = `Ganaste satisfactoriamente la asignatura ${noNotes}`;
-        winWithHonors.innerHTML = `Ganaste con Honores la asignatura ${noNotes}`;
+        minWin.innerHTML = `Ganaste satisfactoriamente la asignatura`;
+        winWithHonors.innerHTML = `Ganaste con Honores la asignatura`;
     }
     
 
